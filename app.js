dd8db650cbf1cf21e35fcbb0da373da4e383bd42
Abhishek <68233166+Codeblocc@users.noreply.github.com>
2022-04-10 15:54:29 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minutesEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+
+const date = new Date("2022-04-17 03:30:00");
+const seconds = Math.floor(date.getTime() / 1000);
+console.log(seconds);
+
+const nbaPlayoffs = date;
+
+function countdown() {
+  const nbaPlayoffsDate = new Date(nbaPlayoffs);
+  const currentDate = new Date();
+
+  const totalSeconds = (nbaPlayoffs - currentDate) / 1000;
+
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 24;
+  const seconds = Math.floor(totalSeconds) % 60;
+
+  console.log(days, hours, minutes, seconds);
+
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = formatTime(hours);
+  minutesEl.innerHTML = formatTime(minutes);
+  secondsEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
+
+countdown();
+
+setInterval(countdown, 1000);
